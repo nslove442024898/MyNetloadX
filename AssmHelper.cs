6c2e62860c86f4f4c9080de326e7cbf06824f01e
nslove442024898 <34982111+nslove442024898@users.noreply.github.com>
2020-10-24 14:51:18 +0800

    20201024

---
+++ AssmHelper.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace MyNetloadX
+{
+    public class FileAndDependentAssemblyList
+    {
+        /// <summary>
+        /// dll引用来源
+        /// </summary>
+        public StringBuilder SourceFile;
+        /// <summary>
+        /// dll文件路径
+        /// </summary>
+        public string File;
+        /// <summary>
+        /// dll内的程序集
+        /// </summary>
+        public List<string> Dependent;
+
+        /// <summary>
+        /// 储存Dll的依赖结构
+        /// </summary>
+        public FileAndDependentAssemblyList(string file = null, string sourceFile = null)
+        {
+            if (sourceFile != null)
+            {
+                SourceFile = new StringBuilder(sourceFile);
+            }
+            File = file;
+            Dependent = new List<string>();
+        }
+    }
+
+    public class DependentAssembly
+    {
+        static string _location;
+        static string _loadPath;
+        static Assembly[] _cadAppAssemblie;
+        static string _dllFile;
+
+        /// <summary>
+        /// 加载dll的和相关的依赖
+        /// </summary>
+        /// <param name="dllFile"></param>
+        public DependentAssembly(string dllFile)
+        {
+            _dllFile = dllFile;
+            //cad程序集的依赖
+            _cadAppAssemblie = AppDomain.CurrentDomain.GetAssemblies();
+
+            //这里是有关CLR的Assembly的搜索路径的过程,
+            //插件目录 "G:\\K01.惊惊连盒\\net35"
+            _location = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            //拖拉文件的路径 "D:\\桌面\\若海的\\test2\\bin\\Debug"
+            _loadPath = Path.GetDirectoryName(_dllFile);
+
+            //运行时靠事件来解决问题,见博客--记得反注释
+            //AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+        }
+
+        /// <summary>
+        /// 依赖的dll列表
+        /// </summary>
+        public FileAndDependentAssemblyList[] AssemblyList()
+        {
+            BuildDependentAssemblyList(_dllFile, null);
+            return _fadaList.ToArray();//数组0后面跟的就是数组0的引用
+        }
+
+        //dll引用表,数组[1]就是[0]的引用
+        static readonly List<FileAndDependentAssemblyList> _fadaList = new List<FileAndDependentAssemblyList>();
+        // 储存dll的引用
+        string _sourceFile;
+        // 每次递归的临时对象
+        static AssemblyName _assemblyName;
+
+        /// <summary>
+        /// 链式查找依赖的dll
+        /// </summary>
+        /// <param name="pa">dll文件路径</param>
+        /// <param name="dlls">首次为null</param>
+        /// <returns>返回在_fadaList集合上</returns>
+        void BuildDependentAssemblyList(string pa, FileAndDependentAssemblyList dlls)
+        {
+            if (dlls == null)
+            {
+                dlls = new FileAndDependentAssemblyList();
+            }
+
+            // 是否已经包含这个路径的程序了
+            if (dlls.Dependent.Contains(pa))
+                return;
+
+            bool reflectionOnlyLoad = true;
+            bool addlies = true;
+            Assembly asm = null;
+
+            // 路径 || 程序名
+            if ((pa.IndexOf(Path.DirectorySeparatorChar, 0, pa.Length) != -1) ||
+                (pa.IndexOf(Path.AltDirectorySeparatorChar, 0, pa.Length) != -1))
+            {
+                dlls = new FileAndDependentAssemblyList(pa, _sourceFile);
+                _sourceFile = pa;
+                // 从这个路径加载程序集,路径
+                asm = Assembly.ReflectionOnlyLoadFrom(pa);
+            }
+            else
+            {
+                //判断cad程序集中是否存在,dll的依赖,不存在则搜索拖拉路径的dll,
+                //如果已经存在cad程序集上面就直接加载就可以了 
+                //(cad程序集的依赖)和(加载的dll程序集的依赖)不一样,就加载,并加入集合,==0直接加载
+                if (_cadAppAssemblie.Count(c => c.FullName == pa) == 0)
+                {
+                    var asdl = _assemblyName.Name + ".dll";
+                    string path1 = Path.Combine(_location, asdl);
+                    string path2 = Path.Combine(_loadPath, asdl);
+                    //如果插件目录和拖拉目录都有,那么拖拉目录会覆盖前面
+                    reflectionOnlyLoad = LoadFrom(path1, ref dlls, ref asm, ref addlies);
+                    reflectionOnlyLoad = LoadFrom(path2, ref dlls, ref asm, ref addlies);
+                }
+                if (reflectionOnlyLoad)
+                {
+                    addlies = false;
+                    dlls.Dependent.Add(pa);
+                    // 反射加载上下文,但不能执行程序集
+                    asm = Assembly.ReflectionOnlyLoad(pa);
+                }
+            }
+
+            if (asm == null)
+            {
+                return;
+            }
+
+            //如果包含的话,表示要插入到这个dll下的 
+            if (addlies)
+            {
+                _fadaList.Add(dlls);
+            }
+
+            // 遍历依赖(所有的引用)，并进行递归
+            var asms = asm.GetReferencedAssemblies();
+            foreach (AssemblyName item in asms)
+            {
+                _assemblyName = item;
+                BuildDependentAssemblyList(item.FullName, dlls);
+            }
+        }
+
+
+        /// <summary>
+        /// 反射加载上下文,执行程序集
+        /// </summary> 
+        bool LoadFrom(string path,
+            ref FileAndDependentAssemblyList dlls,
+            ref Assembly asm,
+            ref bool addlies)
+        {
+            if (File.Exists(path))
+            {
+                dlls = new FileAndDependentAssemblyList(path, _sourceFile);
+                _sourceFile = path;
+                // 反射加载上下文,执行程序集
+                asm = Assembly.ReflectionOnlyLoadFrom(path);
+                addlies = true;
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// 加载依赖的dll
+        /// </summary>
+        public void Load()
+        {
+            LoadDependentAssemblyList(_dllFile);
+        }
+
+        /// <summary>
+        /// 加载依赖的dll
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="assemblies"></param>
+        /// <returns></returns>
+        void LoadDependentAssemblyList(string path)
+        {
+            Assembly asm = null;
+            // 路径 || 程序名
+            if ((path.IndexOf(Path.DirectorySeparatorChar, 0, path.Length) != -1) ||
+                (path.IndexOf(Path.AltDirectorySeparatorChar, 0, path.Length) != -1))
+            {
+                // 从这个路径加载程序集,路径 
+                asm = Assembly.Load(File.ReadAllBytes(path));
+            }
+            else if (_cadAppAssemblie.Count(c => c.FullName == path) == 0)
+            {
+                //判断cad程序集中是否存在,dll的依赖,不存在则搜索拖拉路径的dll,
+                //如果已经存在cad程序集上面就直接加载就可以了
+                //(cad程序集的依赖)和(加载的dll程序集的依赖)不一样,就加载,并加入集合,==0直接加载
+                var asdl = _assemblyName.Name + ".dll";
+                string path1 = Path.Combine(_location, asdl);
+                string path2 = Path.Combine(_loadPath, asdl);
+                //如果插件目录和拖拉目录都有,那么拖拉目录会覆盖前面
+                if (File.Exists(path2))
+                {
+                    // 从这个路径加载程序集,路径
+                    asm = Assembly.Load(File.ReadAllBytes(path2));
+                }
+                else if (File.Exists(path1))
+                {
+                    // 从这个路径加载程序集,路径 
+                    asm = Assembly.Load(File.ReadAllBytes(path1));
+                }
+            }
+
+            if (asm == null)
+            {
+                return;
+            }
+
+            // 遍历依赖(所有的引用)，并进行递归
+            var asms = asm.GetReferencedAssemblies();
+            foreach (AssemblyName item in asms)
+            {
+                _assemblyName = item;
+                LoadDependentAssemblyList(item.FullName);
+            }
+        }
+    }
+
+    public class MyHelper
+    {
+        public static Version GetFileVersionCe(string fileName)
+        {
+            int handle = 0;
+            int length = GetFileVersionInfoSize(fileName, ref handle);
+            Version v = null;
+            if (length > 0)
+            {
+                IntPtr buffer = System.Runtime.InteropServices.Marshal.AllocHGlobal(length);
+                if (GetFileVersionInfo(fileName, handle, length, buffer))
+                {
+                    IntPtr fixedbuffer = IntPtr.Zero;
+                    int fixedlen = 0;
+                    if (VerQueryValue(buffer, "\\\\", ref fixedbuffer, ref fixedlen))
+                    {
+                        byte[] fixedversioninfo = new byte[fixedlen];
+                        System.Runtime.InteropServices.Marshal.Copy(fixedbuffer, fixedversioninfo, 0, fixedlen);
+                        v = new Version(
+                            BitConverter.ToInt16(fixedversioninfo, 10),
+                            BitConverter.ToInt16(fixedversioninfo, 8),
+                            BitConverter.ToInt16(fixedversioninfo, 14),
+                            BitConverter.ToInt16(fixedversioninfo, 12));
+                    }
+                }
+                Marshal.FreeHGlobal(buffer);
+            }
+            return v;
+        }
+
+        [DllImport("version.dll", EntryPoint = "GetFileVersionInfo", SetLastError = true)]
+        private static extern bool GetFileVersionInfo(string filename, int handle, int len, IntPtr buffer);
+        [DllImport("version.dll", EntryPoint = "GetFileVersionInfoSize", SetLastError = true)]
+        private static extern int GetFileVersionInfoSize(string filename, ref int handle);
+        [DllImport("version.dll", EntryPoint = "VerQueryValue", SetLastError = true)]
+        private static extern bool VerQueryValue(IntPtr buffer, string subblock, ref IntPtr blockbuffer, ref int len);
+
+    }
+}
---
+++ MianCls.cs
@@ -0,0 +1,326 @@
+﻿
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace MyNetloadX
+{
+    public class AutoCADNetLoader : IExtensionApplication
+    {
+        public string currentDllPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+        public void Initialize()//初始化程序。
+        {
+            helper.Register2HKCR();
+            string path = Assembly.GetExecutingAssembly().Location;
+            helper.AddCmdtoMenuBar(helper.GetDllCmds(path));
+            Application.SetSystemVariable("SECURELOAD", 0);
+        }
+
+        public void Terminate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Tools
+    {
+        static List<string[]> listDllInfo = new List<string[]>();
+
+        [CommandMethod("MyNetLoader")]
+        public void Dll加载到内存并卸载()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+            Autodesk.AutoCAD.Windows.OpenFileDialog ofd = new Autodesk.AutoCAD.Windows.OpenFileDialog("Dll加载到内存并卸载", "Dll加载到内存并卸载", "dll", "Dll加载到内存并卸载", Autodesk.AutoCAD.Windows.OpenFileDialog.OpenFileDialogFlags.AllowMultiple);
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_AssemblyResolve;
+            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                foreach (var item in ofd.GetFilenames())
+                {
+                    var _cadAppAssemblie = AppDomain.CurrentDomain.GetAssemblies();
+                    var verCurLoad = MyHelper.GetFileVersionCe(item);
+                    var alreadyLoad = listDllInfo.Where(c => c[0] == Path.GetFileNameWithoutExtension(item)).ToList();
+                    DependentAssembly dependentAssembly = new DependentAssembly(item);
+                    if (alreadyLoad.Count == 0)
+                    {
+                        dependentAssembly.Load();
+                        Application.ShowAlertDialog($"首次加载{Path.GetFileNameWithoutExtension(item)} ====> File Version {verCurLoad.ToString()} 成功！！");
+                        listDllInfo.Add(new string[] { Path.GetFileNameWithoutExtension(item), verCurLoad.ToString() });
+                    }
+                    else
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        bool flag = false;
+                        for (int i = 0; i < alreadyLoad.Count; i++)
+                        {
+                            if (alreadyLoad[i][1] == verCurLoad.ToString())
+                            {
+                                Application.ShowAlertDialog($"{alreadyLoad[i][0]},{alreadyLoad[i][1]} 已经在程序中，如需覆盖加载请修改dll的 Assembly FileVersion！！");
+                                flag = true;
+                            }
+                            else sb.AppendLine(alreadyLoad[i][1]);
+                        }
+                        if (flag == false)
+                        {
+                            dependentAssembly.Load();
+                            Application.ShowAlertDialog($"覆盖加载 {Path.GetFileNameWithoutExtension(item)} 成功！！加载的版本为{verCurLoad.ToString()},以下版本不再可用了\n" + sb.ToString());
+                            listDllInfo.Add(new string[] { Path.GetFileNameWithoutExtension(item), verCurLoad.ToString() });
+                        }
+                    }
+
+                }
+            }
+            else Autodesk.AutoCAD.ApplicationServices.Application.ShowAlertDialog("请选择一个或者多个dll文件！");
+        }
+        private Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            return (sender as AppDomain).GetAssemblies().FirstOrDefault(a => a.GetName().FullName.Split(',')[0] == args.Name.Split(',')[0]);
+        }
+
+    }
+    public static class helper
+
+
+    {/// <summary>
+     ///提取所有的命令
+     /// </summary>
+     /// <param name="dllFiles">dll的路径</param>
+     /// <returns></returns>
+        public static List<gcadDllcmd> GetDllCmds(params string[] dllFiles)
+        {
+            List<gcadDllcmd> res = new List<gcadDllcmd>();
+            List<gcadCmds> cmds = new List<gcadCmds>();
+            #region 提取所以的命令
+            for (int i = 0; i < dllFiles.Length; i++)
+            {
+                Assembly ass = Assembly.LoadFile(dllFiles[i]);//反射加载dll程序集
+                var clsCollection = ass.GetTypes().Where(t => t.IsClass && t.IsPublic).ToList();
+                if (clsCollection.Count > 0)
+                {
+                    foreach (var cls in clsCollection)
+                    {
+                        var methods = cls.GetMethods().Where(m => m.IsPublic && m.GetCustomAttributes(true).Length > 0).ToList();
+                        if (methods.Count > 0)
+                        {
+                            foreach (MethodInfo mi in methods)
+                            {
+                                var atts = mi.GetCustomAttributes(true).Where(c => c is CommandMethodAttribute).ToList();
+                                if (atts.Count == 1)
+                                {
+                                    gcadCmds cmd = new gcadCmds(cls.Name, mi.Name, (atts[0] as CommandMethodAttribute).GlobalName, ass.ManifestModule.Name.Substring(0, ass.ManifestModule.Name.Length - 4));
+                                    cmds.Add(cmd);
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+            #endregion
+            if (cmds.Count > 0)
+            {
+                List<string> dllName = new List<string>();
+                foreach (var item in cmds)
+                {
+                    if (!dllName.Contains(item.dllName)) dllName.Add(item.dllName);
+                }
+                foreach (var item in dllName) res.Add(new gcadDllcmd(item, cmds));
+            }
+            return res;
+            //
+        }
+        public static void AddCmdtoMenuBar(List<gcadDllcmd> cmds)
+        {
+            var dllName = Assembly.GetExecutingAssembly().ManifestModule.Name.Substring(0, Assembly.GetExecutingAssembly().ManifestModule.Name.Length - 4);
+            dynamic gcadApp = Application.AcadApplication;
+            dynamic mg = null;
+            dynamic count = gcadApp.MenuGroups.Count;
+            for (int i = 0; i < count; i++) if (gcadApp.MenuGroups.Item(i).Name == "ACAD") mg = gcadApp.MenuGroups.Item(i);
+
+            for (int i = 0; i < mg.Menus.Count; i++) if (mg.Menus.Item(i).Name == dllName) mg.Menus.Item(i).RemoveFromMenuBar();
+            dynamic popMenu = mg.Menus.Add(dllName);
+            for (int i = 0; i < cmds.Count; i++)
+            {
+                var dllPopMenu = popMenu.AddSubMenu(popMenu.Count + 1, cmds[i].DllName);
+                for (int j = 0; j < cmds[i].clsCmds.Count; j++)
+                {
+                    var clsPopMenu = dllPopMenu.AddSubMenu(dllPopMenu.Count + 1, cmds[i].clsCmds[j].clsName);
+                    for (int k = 0; k < cmds[i].clsCmds[j].curClscmds.Count; k++)
+                    {
+                        var methodPopMenu = clsPopMenu.AddMenuItem(clsPopMenu.Count + 1, cmds[i].clsCmds[j].curClscmds[k].cmdName, cmds[i].clsCmds[j].curClscmds[k].cmdMacro + " ");
+                    }
+                }
+            }
+            popMenu.InsertInMenuBar(mg.Menus.Count + 1);
+        }
+
+        /// <summary>
+        /// 将菜单加载到AutoCAD
+        /// </summary>
+        public static void Register2HKCR()
+        {
+            string hkcrKey = HostApplicationServices.Current.UserRegistryProductRootKey;
+            var assName = Assembly.GetExecutingAssembly().Location;
+            var apps_Acad = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(Path.Combine(hkcrKey, "Applications"));
+            if (apps_Acad.GetSubKeyNames().Count(c => c == Path.GetFileNameWithoutExtension(assName)) == 0)
+            {
+                var myNetLoader = apps_Acad.CreateSubKey(Path.GetFileNameWithoutExtension(assName), RegistryKeyPermissionCheck.Default);
+                myNetLoader.SetValue("DESCRIPTION", "加载自定义dll文件", Microsoft.Win32.RegistryValueKind.String);
+                myNetLoader.SetValue("LOADCTRLS", 2, Microsoft.Win32.RegistryValueKind.DWord);
+                //b.注册表键值"LOADCTRLS"控制说明，控制ARX程序的加载方式
+                //0x01：Load the application upon detection of proxy object.
+                //当代理对像被控知时另载相应ARX程序.
+                //0x02：Load the application upon AutoCAD startup.
+
+                //当AutoCAD启动时加载相应ARX程序.
+                //0x04：Load the application upon invocation of a command.
+
+                //当输入命令时加载相应ARX程序.
+                //0x08：Load the application upon request by the user or another application.
+                //当有用户或别的程序请求时加载相应ARX程序.
+                //0x10：Do not load the application.
+                //从不加载该应用程序.
+                //0x20：Load the application transparently.
+
+                //显式加载该应该程序.(不知该项译法是否有误)
+                myNetLoader.SetValue("LOADER", assName, Microsoft.Win32.RegistryValueKind.String);
+                myNetLoader.SetValue("MANAGED", 1, Microsoft.Win32.RegistryValueKind.DWord);
+                Application.ShowAlertDialog(Path.GetFileNameWithoutExtension(assName) + "程序自动加载完成，重启AutoCAD 生效！");
+            }
+            else Application.ShowAlertDialog(Path.GetFileNameWithoutExtension(assName) + "程序已经启动！欢迎使用");
+
+        }
+
+        public static bool CheckFileReadOnly(string fileName)
+        {
+            bool inUse = true;
+            FileStream fs = null;
+            try
+            {
+                fs = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.None);
+                inUse = false;
+            }
+            catch { }
+            return inUse;//true表示正在使用,false没有使用  
+        }
+
+
+        public static void SaveToFile(MemoryStream ms, string fileName)
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+            {
+                byte[] data = ms.ToArray();
+
+                fs.Write(data, 0, data.Length);
+                fs.Flush();
+
+                data = null;
+            }
+        }
+        public static void OpenFile(string fileName)
+        {
+            System.Diagnostics.Process.Start(fileName);
+            System.Diagnostics.Process pro = new System.Diagnostics.Process();
+            pro.EnableRaisingEvents = false;
+            pro.StartInfo.FileName = "rundll32.exe";
+            pro.StartInfo.Arguments = "shell32,OpenAs_RunDLL" + fileName;
+            pro.Start();
+        }
+
+
+    }
+    /// <summary>
+    /// 储存自定义的cad命令的信息的类
+    /// </summary>
+    public class gcadCmds
+    {
+        public string clsName { get; set; }
+        public string cmdName { get; set; }
+        public string cmdMacro { get; set; }
+        public string dllName { get; set; }
+
+        public gcadCmds(string _clsName, string _cmdName, string _macro, string _dllName)
+        {
+            this.dllName = _dllName;
+            this.clsName = _clsName;
+            this.cmdMacro = _macro;
+            this.cmdName = _cmdName;
+        }
+
+    }
+    /// <summary>
+    /// 储存包含自定命令的类
+    /// </summary>
+    public class gcadClscmd
+    {
+        public string clsName { get; set; }
+
+        public string dllName { get; set; }
+
+        public bool HasGcadcmds { get; set; }
+
+        public List<gcadCmds> curClscmds { get; set; }
+
+        public gcadClscmd(string _clsName, List<gcadCmds> cmds)
+        {
+            this.clsName = _clsName;
+            this.dllName = cmds.First().dllName;
+            var clsCmds = cmds.Where(c => c.clsName == this.clsName).ToList();
+            if (clsCmds.Count > 0)
+            {
+                this.HasGcadcmds = true;
+                this.curClscmds = new List<gcadCmds>();
+                foreach (var item in clsCmds)
+                {
+                    if (item.clsName == this.clsName) this.curClscmds.Add(item);
+                }
+
+            }
+            else this.HasGcadcmds = false;
+        }
+    }
+    /// <summary>
+    /// 储存每个dll类的
+    /// </summary>
+    public class gcadDllcmd
+    {
+        public string DllName { get; set; }
+        public bool HasGcadcls { get; set; }
+        public List<gcadClscmd> clsCmds { get; set; }
+        public List<gcadCmds> curDllcmds { get; set; }
+        public gcadDllcmd(string _dllname, List<gcadCmds> cmds)
+        {
+            this.DllName = _dllname;
+            var curDllcmds = cmds.Where(c => c.dllName == this.DllName).ToList();
+            if (curDllcmds.Count > 0)
+            {
+                this.HasGcadcls = true;
+                this.curDllcmds = curDllcmds;
+                List<string> listClsName = new List<string>();
+                foreach (gcadCmds item in this.curDllcmds)
+                {
+                    if (!listClsName.Contains(item.clsName)) listClsName.Add(item.clsName);
+                }
+                this.clsCmds = new List<gcadClscmd>();
+                foreach (var item in listClsName)
+                {
+                    gcadClscmd clsCmds = new gcadClscmd(item, this.curDllcmds.Where(c => c.clsName == item).ToList());
+                    this.clsCmds.Add(clsCmds);
+                }
+
+
+            }
+            else this.HasGcadcls = false;
+        }
+
+
+    }
+}
